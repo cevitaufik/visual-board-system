d93ae65ea7124b3a1ff82e22b2508fe7c86ac734
Cevi Taufik <77260222+cevitaufik@users.noreply.github.com>
2022-02-08 22:25:48 +0700

    memperbaiki database factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,6 @@ class UserFactory extends Factory
             'about' => $this->faker->sentence(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'status' => true,
-            'profile_img' => 'profile_image/operator.jpg',
         ];
     }
 
